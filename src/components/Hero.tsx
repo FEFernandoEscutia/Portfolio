2d0f79318a32b759f9f455bb6605508ea95afe44
FEFernandoEscutia <134465084+FEFernandoEscutia@users.noreply.github.com>
2024-11-27 07:50:00 -0700

    creating the hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,31 @@
+import HeroImg from "../assets/Hero4.0.png";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+
+export const Hero = () => {
+  return (
+    <div className="bg-emerald-100 py-24">
+      <div className="align-element grid md:grid-cols-2 items-center gap-8">
+        <article>
+          <h1 className="text-7xl font-mono tracking-wider">I'm Fernando</h1>
+          <p className="mt-4 text-4xl text-slate-700 capitalize tracking-wide font-mono">
+            Full-Stack Developer
+          </p>
+          <p className="mt-2 text-lg text-slate-700 capitalize tracking-wide font-mono">
+            Turning ideas into interactive reality
+          </p>
+          <div className="flex gap-x-4 mt-4">
+            <a href="https://github.com/FEFernandoEscutia">
+              <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300"/>
+            </a>
+            <a href="https://www.linkedin.com/in/fernando-escutia-75901125b/">
+              <FaLinkedin className="h-8 w-8 text-slate-500 hover:text-black duration-300 "/>
+            </a>
+          </div>
+        </article>
+        <article className="hidden md:block">
+          <img src={HeroImg} className="h-80 lg:h-95"/>
+        </article>
+      </div>
+    </div>
+  );
+};
